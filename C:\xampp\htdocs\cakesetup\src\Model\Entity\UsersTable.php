91bc91c47cf6a68e1dd6fbd50d9ecc32fa3f09b5
rahimi9 <44663519+rahimi9@users.noreply.github.com>
2019-04-16 14:07:24 +0800

    Create C:\xampp\htdocs\cakesetup\src\Model\Entity\UsersTable.php

---
+++ C:\xampp\htdocs\cakesetup\src\Model\Entity\UsersTable.php
@@ -0,0 +1,114 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Users Model
+ *
+ * @method \App\Model\Entity\User get($primaryKey, $options = [])
+ * @method \App\Model\Entity\User newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\User[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\User|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\User patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\User[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\User findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @mixin \Cake\ORM\Behavior\TimestampBehavior
+ */
+class UsersTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('users');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->addBehavior('Timestamp');
+    }
+
+    public function checkPassword($password, array $context)
+    {
+        if (!preg_match("#[A-Z]#", $password)) {
+            return false;
+        }
+
+        if (!preg_match("#[a-z]#", $password)) {
+            return false;
+        }
+
+        if (!preg_match("#[0-9]#", $password)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->nonNegativeInteger('id')
+            ->allowEmptyString('id', 'create');
+
+        $validator
+            ->scalar('username')
+            ->maxLength('username', 255)
+            ->requirePresence('username', 'create')
+            ->allowEmptyString('username', false)
+            ->add('username', 'valid', ['rule' => 'alphaNumeric', 'message' => 'Only accept character & number']);
+
+        $validator
+            ->scalar('password')
+            ->maxLength('password', 255)
+            ->requirePresence('password', 'create')
+            ->allowEmptyString('password', false)
+            ->add('password', 'custom', ['rule' => [$this, 'checkPassword'], 'message' => 'Must have at least 1 uppercase, 1 lowercase, 1 number'])
+            ->add('password', 'valid', ['rule' => 'containsNonAlphaNumeric', 'message' => 'Must have at least 1 special character']);
+            
+        $validator
+            ->email('email')
+            ->requirePresence('email', 'create')
+            ->allowEmptyString('email', false);
+
+        $validator
+            ->scalar('role')
+            ->maxLength('role', 20)
+            ->allowEmptyString('role', false)
+            ->add('role', 'valid', ['rule' => 'numeric', 'message' => 'Only accept numeric']);
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['username']));
+        $rules->add($rules->isUnique(['email']));
+
+        return $rules;
+    }
+}
